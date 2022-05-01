d296253311fdeff464dcf5f456a66b8660c7f45c
mekaiser <kaiser331042@gmail.com>
2022-05-01 21:45:37 +0600

    ongoing

--- components/Header.js
+++ components/Header.js
@@ -19,6 +19,7 @@ import HeaderIcon from "./HeaderIcon";
 
 export default function Header() {
   const {session} = useSession();
+  console.log("session", session);
 
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
