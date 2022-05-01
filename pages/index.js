add1ec516482e52265f0977b6812c71e0f90f768
mekaiser <kaiser331042@gmail.com>
2022-05-01 21:46:46 +0600

    ongoing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Login from '../components/Login'
+import Sidebar from '../components/Sidebar'
 
 export default function Home({session}) {
   if(!session) return <Login/>
@@ -15,7 +16,8 @@ export default function Home({session}) {
       <Header/>
 
     <main>
-      {/* Sidebar */}
+      <Sidebar/>
+      
       {/* Feed */}
       {/* Widgets */}
 
